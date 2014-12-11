56de47a3409ec8c7a47f508c17b3e1410ed7fe10
Jan Wrona <xwrona00@stud.fit.vutbr.cz>
2014-12-11 01:39:24 +0100

    INSTALL.sh: libraries are now downloadig

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,2 +1,30 @@
-cd psutil-2.1.3
-python setup.py install --user
+#author: Jan Wrona
+#email: <xwrona00@stud.fit.vutbr.cz>
+
+libs=("psutil-2.1.3"
+      "networkx-1.9.1")
+urls=("https://pypi.python.org/packages/source/p/psutil/psutil-2.1.3.tar.gz"
+      "https://pypi.python.org/packages/source/n/networkx/networkx-1.9.1.tar.gz")
+
+if [ "${#libs[@]}" -ne "${#urls[@]}" ]
+then
+    echo "Error: |libs| != |urls|"
+    exit 1
+fi
+
+for (( i=0; i<"${#libs[@]}"; i++ ));
+do
+    if [ ! -d "${libs[$i]}" ]
+    then
+	echo "downloading ${libs[$i]}"
+        wget -O "${libs[$i]}.tar.gz" "${urls[$i]}" 2> /dev/null
+        tar -xf "${libs[$i]}.tar.gz"
+        rm -f "${libs[$i]}.tar.gz"
+    fi
+
+    cd "${libs[$i]}"
+    echo "installing ${libs[$i]}"
+    python2 setup.py install --user > /dev/null
+    cd ..
+    rm -rf "${libs[$i]}"
+done
